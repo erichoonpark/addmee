dae3704c6a8cc6851fad026fbae30b4fb41dd822
Eric Park <eric.park@salesforce.com>
2016-09-16 16:04:08 -0700

    Post/Comment Model Generation

---
+++ db/migrate/20160916230230_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :description
+      t.string :cost
+      t.integer :openspots
+      t.boolean :approval
+
+      t.timestamps null: false
+    end
+  end
+end
