4c71622d509356c16e29c42d88e8ce11dea77201
Eric Park <eric.park@salesforce.com>
2016-09-18 00:39:15 -0700

    Category Model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
   def new
+    # @post = Post.new
     @post = Post.new
+    @categories = Category.all.map{|c| [ c.name, c.id ] }
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :description, :cost, :openspots, :approval))
+    @post = Post.new(params.require(:post).permit(:title, :description, :cost, :openspots, :approval, :category_id))
+    @post.category_id = params['post']['category_id']
     @post.user = current_user
      if @post.save
        flash[:notice] = "Post was saved."
@@ -17,6 +20,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    @post.category_id = params['post']['category_id']
      if @post.update_attributes(params.require(:post).permit(:title, :description))
        flash[:notice] = "Post was updated."
        redirect_to @post
@@ -28,6 +32,7 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+    @post.category_id = params['post']['category_id']
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917194201) do
+ActiveRecord::Schema.define(version: 20160917215024) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -31,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160917194201) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "category_id"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
